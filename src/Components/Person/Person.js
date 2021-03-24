0712567768b3514e022c739a02f078e704ef4b6e
tanmayhinge <hingetanmay@gmail.com>
2021-03-24 13:04:06 +0530

    dynamic content and some-styling

--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import classes from './Person.module.css';
 
 const person = props => {
     return(
-        <div>
-            <input type="text" onChange={props.nameChanged} value={props.name} />
-            <input type="number" onChange={props.ageChanged} value={props.age} />
-            {props.name}
-            {props.age}
+        <div >
+            <p             
+            className={classes.Input}>Name: 
+            <input type="text" 
+            onChange={props.nameChanged} 
+            value={props.name}
+
+            /></p>
+
+            <p
+            className={classes.Input}>Age: 
+            <input type="number" 
+            onChange={props.ageChanged} 
+            value={props.age} 
+
+            /></p>
+            <div className ={classes.Person}>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            </div>
+
+            
         </div>
     )
 }
